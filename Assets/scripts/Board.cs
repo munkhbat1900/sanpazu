bbe6b52aea847c968d84145dc60d16edb872ab99
Avirmed Munkhbat <avirmed_munkhbat@cyberagent.co.jp>
2014-09-26 12:53:04 +0900

    initial

---
+++ Assets/scripts/Board.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	// board size at the X-axis
+	public int BOARD_SIZE_X = 7;
+	// board size at the Y-axis
+	public int BOARD_SIZE_Y = 5;
+
+	public void initBoard() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("it is called");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
